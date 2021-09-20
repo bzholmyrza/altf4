6c9c9b1b4ebabe6be2490c6ff4912260ab7d4624
Beibarys Zholmyrza <beibaryszholmyrza@gmail.com>
2021-09-20 18:44:00 +0600

    Added: websockets

--- main.go
+++ main.go
@@ -3,23 +3,46 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/websocket"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
 // Starts the program
 func main() {
-	doEvery(1 * time.Second, sum)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/ws", ws)
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
 
-// Make request every second
-func doEvery(d time.Duration, f func(time.Time)) {
-	for x := range time.Tick(d) {
-		f(x)
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func ws(w http.ResponseWriter, r *http.Request) {
+	conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+	ch := make(chan string)
+	go volume(ch)
+	for {
+		// Write message back to browser
+		err := conn.WriteMessage(1, []byte(<-ch))
+		time.Sleep(time.Second)
+		if err != nil {
+			return
+		}
 	}
 }
 
@@ -30,38 +53,36 @@ type Response struct {
 	Asks         [][]string `json:"asks"`
 }
 
-// Takes data from api, processes it and displays it in the console
-func sum(t time.Time) {
+func volume(ch chan string) strings.Builder {
 	response, err := http.Get("https://api.binance.com/api/v3/depth?symbol=MANABTC&limit=10")
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
-
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	var res Response
 	json.Unmarshal(responseData, &res)
 
 	var sumBid float64
 	var sumAsk float64
-	for i := 0; i < 7; i++ {
-		bid, err := strconv.ParseFloat(res.Bids[0][0], 64)
+
+	for {
+		bid, err := strconv.ParseFloat(res.Bids[0][1], 64)
 		if err != nil {
 			log.Fatal(err)
 		}
 		sumBid += bid
 
-		ask, err := strconv.ParseFloat(res.Bids[0][0], 64)
+		ask, err := strconv.ParseFloat(res.Bids[0][1], 64)
 		if err != nil {
 			log.Fatal(err)
 		}
 		sumAsk += ask
 
-		fmt.Printf("ID: %v, sumBid = %v, sumAsk = %v\n", res.LastUpdateID, sumBid, sumBid)
+		ch <- fmt.Sprintf("ID: %v, sumBid = %v, sumAsk = %v", res.LastUpdateID, sumBid, sumBid)
+		log.Printf("DATA:\t ID: %v, sumBid = %v, sumAsk = %v\n", res.LastUpdateID, sumBid, sumBid)
 	}
-	fmt.Println("------------------------------------------")
 }
